eb12c8310dfe3b0635638f96603583e995dedd97
anagornyuk <anagorniuk@adyax.com>
2018-12-20 14:50:17 +0200

    log in button chenges form to login form

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 function ShowLogin(){
-	alert("It works!");
+	// alert("It works!");
+	document.getElementById('signUpForm').className = "hidden";
+	document.getElementById('loginForm').className = "visible";
+	document.getElementById('signup').classList.remove("checked");
+	document.getElementById('signup').classList.add("unchecked");
+	document.getElementById('login').classList.remove("unchecked");
+	document.getElementById('login').classList.add("checked");
+	// document.getElementById('loginForm')
 }
