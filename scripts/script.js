b5896fd2e8015b7888cef2197042e539bf8f542e
anagornyuk <anagorniuk@adyax.com>
2018-12-20 16:25:05 +0200

    both buttons work

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,17 @@
 function ShowLogin(){
-	// alert("It works!");
 	document.getElementById('signUpForm').className = "hidden";
 	document.getElementById('loginForm').className = "visible";
 	document.getElementById('signup').classList.remove("checked");
 	document.getElementById('signup').classList.add("unchecked");
 	document.getElementById('login').classList.remove("unchecked");
 	document.getElementById('login').classList.add("checked");
-	// document.getElementById('loginForm')
+}
+function ShowSignUp(){
+	document.getElementById('signUpForm').className = "visible";
+	document.getElementById('loginForm').className = "hidden";
+	document.getElementById('login').classList.remove("checked");
+	document.getElementById('login').classList.add("unchecked");
+	document.getElementById('signup').classList.remove("unchecked");
+	document.getElementById('signup').classList.add("checked");
+	
 }
